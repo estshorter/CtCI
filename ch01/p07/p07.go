f29c6cd4d456e99c6b4ea92d611af250ba613042
estshorter <1430311+estshorter@users.noreply.github.com>
2020-02-15 21:11:17 +0900

    Implement ch01-p07

--- ch01/p07/p07.go
+++ ch01/p07/p07.go
@@ -1,5 +1,28 @@
 package p07
 
-func RotationMatrix(mat [][]int, int n, bool positiveRotation) {
+// RotateMatrix rotate matrix
+func RotateMatrix(mat [][]int, n int, positiveRotation bool) [][]int {
+	for layer := 0; layer < n/2; layer++ {
+		for i := 0; i < n-2*layer-1; i++ {
+			top := &mat[layer][layer+i]
+			right := &mat[layer+i][n-1-layer]
+			bottom := &mat[n-1-layer][n-i-1-layer]
+			left := &mat[n-1-i-layer][layer]
 
+			if positiveRotation {
+				tmp := *top
+				*top = *left
+				*left = *bottom
+				*bottom = *right
+				*right = tmp
+			} else {
+				tmp := *top
+				*top = *right
+				*right = *bottom
+				*bottom = *left
+				*left = tmp
+			}
+		}
+	}
+	return mat
 }
---
+++ ch01/p07/p07_test.go
@@ -0,0 +1,43 @@
+package p07
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRotateMatrix(t *testing.T) {
+	type args struct {
+		mat              [][]int
+		n                int
+		positiveRotation bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"2x2p",
+			args{[][]int{{1, 2}, {3, 4}}, 2, true},
+			[][]int{{3, 1}, {4, 2}},
+		},
+		{"2x2n",
+			args{[][]int{{1, 2}, {3, 4}}, 2, false},
+			[][]int{{2, 4}, {1, 3}},
+		},
+		{"5x5p",
+			args{[][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}, 5, true},
+			[][]int{{21, 16, 11, 6, 1}, {22, 17, 12, 7, 2}, {23, 18, 13, 8, 3}, {24, 19, 14, 9, 4}, {25, 20, 15, 10, 5}},
+		},
+		{"5x5n",
+			args{[][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}, 5, false},
+			[][]int{{5, 10, 15, 20, 25}, {4, 9, 14, 19, 24}, {3, 8, 13, 18, 23}, {2, 7, 12, 17, 22}, {1, 6, 11, 16, 21}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RotateMatrix(tt.args.mat, tt.args.n, tt.args.positiveRotation); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RotateMatrix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
