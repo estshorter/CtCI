294207485ac0565898c27e133a5b4df18b060fcc
estshorter <1430311+estshorter@users.noreply.github.com>
2020-02-18 22:19:26 +0900

    Add another solution for ch02 p06

--- ch02/p06/p06.go
+++ ch02/p06/p06.go
@@ -1,6 +1,9 @@
 package p06
 
-import "ctci/container/list"
+import (
+	"ctci/container/list"
+	"ctci/container/stack"
+)
 
 // IsPalindrome checks if a list l is a palindrome
 func IsPalindrome(l *list.List) bool {
@@ -16,16 +19,49 @@ func IsPalindrome(l *list.List) bool {
 
 // IsPalindrome2 checks if a list l is a palindrome
 func IsPalindrome2(l *list.List) bool {
-	slice := make([]int, l.Len())
-	i := 0
-	for e := l.Front(); !l.IsConsumed(e); e = e.Next {
-		slice[i] = e.Value
-		i++
+	fast, slow := l.Front(), l.Front()
+	stack := stack.New()
+	for ; !l.IsConsumed(fast) && !l.IsConsumed(fast.Next); fast = fast.Next.Next {
+		stack.Push(slow.Value)
+		slow = slow.Next
 	}
-	for i := 0; i < l.Len()/2; i++ {
-		if slice[i] != slice[len(slice)-1-i] {
+	if l.IsConsumed(fast.Next) {
+		slow = slow.Next
+	}
+	for ; !l.IsConsumed(slow); slow = slow.Next {
+		if slow.Value != stack.Pop() {
 			return false
 		}
 	}
 	return true
+}
+
+// IsPalindrome3 checks if a list l is a palindrome
+func IsPalindrome3(l *list.List) bool {
+	ret := isPalindromeRecurse(l.Front(), l.Len())
+	return ret.isPalindrome
+}
+
+func isPalindromeRecurse(e *list.Element, len int) *result {
+	switch len {
+	case 0: //even
+		return &result{true, e}
+	case 1: //odd
+		return &result{true, e.Next}
+	}
+
+	ret := isPalindromeRecurse(e.Next, len-2)
+	if !ret.isPalindrome {
+		return ret
+	}
+	if e.Value != ret.e.Value {
+		ret.isPalindrome = false
+	}
+	ret.e = ret.e.Next
+	return ret
+}
+
+type result struct {
+	isPalindrome bool
+	e            *list.Element
 }
--- ch02/p06/p06_test.go
+++ ch02/p06/p06_test.go
@@ -59,3 +59,31 @@ func TestIsPalindrome2(t *testing.T) {
 		})
 	}
 }
+
+func TestIsPalindrome3(t *testing.T) {
+	l1 := list.InitBySlice([]int{1, 0, 7, 0, 1})
+	l2 := list.InitBySlice([]int{1, 0, 0, 1})
+	l3 := list.InitBySlice([]int{1, 0, 1, 1})
+	l4 := list.InitBySlice([]int{1, 0, 5, 1, 1})
+
+	type args struct {
+		l *list.List
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"case1", args{l1}, true},
+		{"case1", args{l2}, true},
+		{"case1", args{l3}, false},
+		{"case1", args{l4}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPalindrome3(tt.args.l); got != tt.want {
+				t.Errorf("IsPalindrome3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
