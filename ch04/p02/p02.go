2a63e8dfab4cb02f2181ffe27b93bb940e06ae1d
estshorter <1430311+estshorter@users.noreply.github.com>
2020-02-23 20:19:19 +0900

    Modify ch04-p02

--- ch04/p02/p02.go
+++ ch04/p02/p02.go
@@ -6,18 +6,12 @@ import (
 
 // CreateMinimalBST solves ch04-p02
 func CreateMinimalBST(s []int) *tree.Tree {
-	switch len(s) {
-	case 1:
-		return &tree.Tree{Value: s[0]}
-	case 2:
-		t := &tree.Tree{Value: s[1]}
-		t.Left = &tree.Tree{Value: s[0]}
-		return t
-	default:
-		mid := len(s) / 2
-		t := &tree.Tree{Value: s[mid]}
-		t.Left = CreateMinimalBST(s[0:mid])
-		t.Right = CreateMinimalBST(s[mid+1 : len(s)])
-		return t
+	if len(s) == 0 {
+		return nil
 	}
+	mid := len(s) / 2
+	t := &tree.Tree{Value: s[mid]}
+	t.Left = CreateMinimalBST(s[0:mid])
+	t.Right = CreateMinimalBST(s[mid+1 : len(s)])
+	return t
 }
