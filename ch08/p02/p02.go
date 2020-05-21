ef3f046d39beb1c98fc6889df44d85253c058327
estshorter <1430311+estshorter@users.noreply.github.com>
2020-05-21 21:50:14 +0900

    Add ch08-p02

--- ch08/p02/p02.go
+++ ch08/p02/p02.go
@@ -1,8 +1,12 @@
 package p02
 
+import (
+	"github.com/golang-collections/collections/stack"
+)
+
 // Point defines 2D points
 type Point struct {
-	x, y int
+	Row, Col int
 }
 
 func getPath(maze [][]bool) []Point {
@@ -36,4 +40,57 @@ func getPathInternal(maze [][]bool, row, col int, path []Point, memo map[Point]b
 		return true, append(pathRet, p)
 	}
 	return false, path
+}
+
+func getPathStack(maze [][]bool) []Point {
+	if maze == nil || len(maze) == 0 {
+		return nil
+	}
+
+	memo := make(map[Point]bool)
+	s := stack.New()
+	sPath := stack.New()
+	s.Push(Point{len(maze) - 1, len(maze[0]) - 1})
+	for {
+		p, ok := s.Pop().(Point)
+		if !ok {
+			return nil
+		}
+
+		if p.Row < 0 || p.Col < 0 || !maze[p.Row][p.Col] {
+			if (sPath.Peek().(Point).Row - p.Row) == 0 {
+				continue
+			}
+			n := s.Peek().(Point).Row - p.Row + 1
+			for i := 0; i < n; i++ {
+				sPath.Pop()
+			}
+			continue
+		}
+		if _, ok := memo[p]; ok {
+			if (sPath.Peek().(Point).Row - p.Row) == 0 {
+				continue
+			}
+			n := s.Peek().(Point).Row - p.Row + 1
+			for i := 0; i < n; i++ {
+				sPath.Pop()
+			}
+			continue
+		}
+		if (p.Row == 0) && (p.Col == 0) {
+			sPath.Push(p)
+			break
+		}
+
+		s.Push(Point{p.Row - 1, p.Col})
+		s.Push(Point{p.Row, p.Col - 1})
+		sPath.Push(p)
+	}
+
+	path := make([]Point, sPath.Len())
+	n := sPath.Len()
+	for i := 0; i < n; i++ {
+		path[i] = sPath.Pop().(Point)
+	}
+	return path
 }
--- ch08/p02/p02_test.go
+++ ch08/p02/p02_test.go
@@ -36,3 +36,36 @@ func Test_getPathNaive(t *testing.T) {
 		})
 	}
 }
+
+func Test_getPathStack(t *testing.T) {
+	const row = 5
+	const col = 4
+	maze := make([][]bool, row)
+	for r := 0; r < row; r++ {
+		maze[r] = make([]bool, col)
+		for c := 0; c < col; c++ {
+			maze[r][c] = true
+		}
+	}
+	maze[1][0] = false
+	maze[1][1] = false
+	want1 := []Point{{0, 0}, {0, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {4, 3}}
+
+	type args struct {
+		maze [][]bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Point
+	}{
+		{"1", args{maze}, want1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPathStack(tt.args.maze); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getPathStack() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
