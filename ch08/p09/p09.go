c1b5ed93b0d7ad8c6fac06fe7075469b605d35b2
estshorter <1430311+estshorter@users.noreply.github.com>
2020-05-24 19:33:18 +0900

    Fix ch08 p09

--- ch08/p09/p09.go
+++ ch08/p09/p09.go
@@ -3,16 +3,15 @@ package p09
 func addParen(list []string, leftRem, rightRem int, str []rune, index int) []string {
 	if leftRem < 0 || rightRem < leftRem {
 		return list
+	} else if leftRem == 0 && rightRem == 0 {
+		return append(list, string(str))
 	}
-	if leftRem == 0 && rightRem == 0 {
-		list = append(list, string(str))
-	} else {
-		str[index] = '('
-		list = addParen(list, leftRem-1, rightRem, str, index+1)
 
-		str[index] = ')'
-		list = addParen(list, leftRem, rightRem-1, str, index+1)
-	}
+	str[index] = '('
+	list = addParen(list, leftRem-1, rightRem, str, index+1)
+
+	str[index] = ')'
+	list = addParen(list, leftRem, rightRem-1, str, index+1)
 	return list
 }
 
