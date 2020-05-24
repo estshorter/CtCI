70e023e4f9eb4922ea629f280da5ca59999926e5
estshorter <1430311+estshorter@users.noreply.github.com>
2020-05-24 17:43:40 +0900

    Add ch08 p09

---
+++ ch08/p09/p09.go
@@ -0,0 +1,27 @@
+package p09
+
+func permutation(input string) []string {
+	return permHelper("", len(input), buildRuneCount(input), []string{})
+}
+
+func permHelper(prefix string, reminder int, counter map[rune]int, result []string) []string {
+	if reminder == 0 {
+		return append(result, prefix)
+	}
+	for r, c := range counter {
+		if c > 0 {
+			counter[r]--
+			result = permHelper(prefix+string(r), reminder-1, counter, result)
+			counter[r]++
+		}
+	}
+	return result
+}
+
+func buildRuneCount(input string) map[rune]int {
+	counter := make(map[rune]int)
+	for _, r := range input {
+		counter[r]++
+	}
+	return counter
+}
---
+++ ch08/p09/p09_test.go
@@ -0,0 +1,64 @@
+package p09
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildRuneCount(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[rune]int
+	}{
+		{"1", args{"abc"}, map[rune]int{'a': 1, 'b': 1, 'c': 1}},
+		{"2", args{"aabbcc"}, map[rune]int{'a': 2, 'b': 2, 'c': 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildRuneCount(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildRuneCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_permutation(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"1", args{"abc"}, []string{"abc", "acb", "bac", "bca", "cba", "cab"}},
+		{"2", args{"aaaaa"}, []string{"aaaaa"}},
+		{"3", args{"aabb"}, []string{"aabb", "abba", "abab", "baab", "baba", "bbaa"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := permutation(tt.args.input); !equal(got, tt.want) {
+				t.Errorf("permutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func equal(ss1, ss2 []string) bool {
+	if len(ss1) != len(ss2) {
+		return false
+	}
+	counter1 := make(map[string]int)
+	counter2 := make(map[string]int)
+	for _, s1 := range ss1 {
+		counter1[s1]++
+	}
+	for _, s2 := range ss2 {
+		counter2[s2]++
+	}
+	return reflect.DeepEqual(counter1, counter2)
+}
